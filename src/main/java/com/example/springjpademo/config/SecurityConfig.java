5868e391f58c8a90289829f3d3cf727498927492
unstar <wox0715@naver.com>
2024-05-21 17:41:14 +0900

    security deprecated로 인하여 임시로 csrf disable 처리함

--- src/main/java/com/example/springjpademo/config/SecurityConfig.java
+++ src/main/java/com/example/springjpademo/config/SecurityConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -15,7 +17,9 @@ public class SecurityConfig{
     //WebSecurityConfigurerAdapter.configure 대응
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-      return http.build();
+        //spring security deprecated로 csrf생성이 안되는것같음, 화면에서도 널처리해놓고, 일단 권장사항으로 csrf disable
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
     };
 
     @Bean
