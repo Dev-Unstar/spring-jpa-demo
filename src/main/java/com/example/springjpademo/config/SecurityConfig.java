9312bdc174ee64ceef8e87efe6af6c92d239fbf9
unstar <wox0715@naver.com>
2024-05-22 12:37:21 +0900

    Security 설정

--- src/main/java/com/example/springjpademo/config/SecurityConfig.java
+++ src/main/java/com/example/springjpademo/config/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.example.springjpademo.config;
 
+import com.example.springjpademo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
@@ -9,16 +14,30 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
+
     //WebSecurityConfigurerAdapter.configure 대응
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         //spring security deprecated로 csrf생성이 안되는것같음, 화면에서도 널처리해놓고, 일단 권장사항으로 csrf disable
         http.csrf(AbstractHttpConfigurer::disable);
+        //Spring Security 6.1 메서드체이닝이 아래와같이 람다로 바뀜
+        http.formLogin(formLogin -> formLogin
+                    .loginPage("/members/login")
+                    .defaultSuccessUrl("/")
+                    .usernameParameter("email")
+                    .failureUrl("/members/login/error")
+        );
+        http.logout(logout -> logout
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                .logoutSuccessUrl("/")
+        );
         return http.build();
     };
 
@@ -26,4 +45,10 @@ public class SecurityConfig{
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 }
